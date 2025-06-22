f43f72d8023456620178c12e64d0d4ae93bfa41e
marine64737 <tkdgh01248@naver.com>
2025-06-22 21:44:33 +0900

    Coding test

---
+++ Java/BAEKJOON/10818_ 최소, 최대.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) throws IOException {
+    // test your function here
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    String[] inputs = br.readLine().split(" ");
+    List<Integer> nums = new ArrayList<>();
+    for (int i = 0; i< N; i++){
+        int num = Integer.parseInt(inputs[i]);
+        nums.add(num);
+    }
+    System.out.println(Collections.min(nums)+" "+Collections.max(nums));
+
+}
